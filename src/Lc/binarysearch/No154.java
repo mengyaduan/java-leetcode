b126d0198c0ad91d738f2b30447cc39079a71d5f
yameng.dym <yameng.dym@autonavi.com>
2021-11-17 23:53:07 +0800

    add no.540

--- src/Lc/binarysearch/No154.java
+++ src/Lc/binarysearch/No154.java
@@ -5,7 +5,7 @@ import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 /**
- * @see <a href="https://leetcode.com/problems/find-minimum-in-rotated-sorted-array-ii/"</a>
+ * @see <a href="https://leetcode.com/problems/find-minimum-in-rotated-sorted-array-ii/">154:找到旋转数组中的最小值</a>
  **/
 public class No154 {
 
@@ -15,9 +15,9 @@ public class No154 {
 
     public int findMin(int[] nums) {
         int len = nums.length;
-        if (len == 1) {
-            return nums[0];
-        }
+//        if (len == 1) {
+//            return nums[0];
+//        }
         int left = 0;
         int right = len - 1;
         // 已经升序了，直接返回最左
@@ -47,6 +47,9 @@ public class No154 {
         return result;
     }
 
+    /**
+     * 判定左边界收缩，还是右边界收缩
+     **/
     public boolean isInFirst(int[] nums, int index, int target) {
         return target >= nums[index];
     }
@@ -64,14 +67,58 @@ public class No154 {
                 {new int[]{2, 5, 6, 7, 8, 9, 0}, 0},
                 {new int[]{2, 2, 2, 2, 2, 2, 2}, 2},
                 {new int[]{10, 1, 10, 10, 10}, 1},
+                {new int[]{1, 1, 1, 2, 1}, 1},
 
         };
         return data;
     }
 
+    public int findMinAnswer(int[] nums) {
+        int low = 0;
+        int high = nums.length - 1;
+        while (low < high) {
+            int pivot = low + (high - low) / 2;
+            if (nums[pivot] < nums[high]) {
+                high = pivot;
+            } else if (nums[pivot] > nums[high]) {
+                low = pivot + 1;
+            } else {
+                high -= 1;
+            }
+        }
+        return nums[low];
+    }
+
+    public int findMin2(int[] nums) {
+        int low = 0;
+        int high = nums.length - 1;
+        while (low < high) {
+            int pivot = low + (high - low) / 2;
+            if (nums[pivot] > nums[low]) {
+                low = pivot;
+            } else if (nums[pivot] < nums[low]) {
+                high = pivot - 1;
+            } else {
+                low += 1;
+            }
+        }
+        return nums[low];
+    }
+
+
+    // 测试点
+
+    /**
+     * 1. 只有一个元素
+     * 2. 已经是升序的了
+     * 3. 最右边一个是min
+     * 4. 所有一样大
+     * 5. 二分法中间值的左右一边大，需要判定是往左还是往右
+     **/
+
     @Test(description = "单测", dataProvider = "cases")
     public void test(int[] nums, int expected) {
-        int res = findMin(nums);
+        int res = findMinAnswer(nums);
         Assert.assertEquals(res, expected);
     }
 }
---
+++ src/Lc/binarysearch/No540.java
@@ -0,0 +1,88 @@
+package Lc.binarysearch;
+
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+/**
+ * @see <a href="https://leetcode.com/problems/single-element-in-a-sorted-array/">504:有序数组中找单一元素</a>
+ **/
+public class No540 {
+
+    /**
+     * 解题思路：
+     * 1. 双指针方法：直接双指针，每次加2，直到左右不一致。但是注意！此时会有++后溢出的场景（优于二分法）
+     * 2. 二分法：
+     * - 找到中间值，如果中间值和左右都不一样，则中间值为单一元素，直接return
+     * - 中间值是一对数字的左值，则在单侧奇数情况下，目标元素在中间值的左侧；如果单侧偶数，则目标元素在中间值的右侧
+     * - 中间值是一对数字的右值，则在单侧奇数情况下，目标元素在中间值的右侧；如果单侧偶数，则目标元素在中间值的左侧
+     * - 这里注意，收缩窗口的时候需要将与中间值相同的数字跳过
+     **/
+
+    public int singleNonDuplicate(int[] nums) {
+        if (nums.length == 1) {
+            return nums[0];
+        }
+        int left = 0;
+        int right = 1;
+        while (right < nums.length) {
+            if (nums[left] != nums[right]) {
+                return nums[left];
+            }
+            left += 2;
+            right += 2;
+        }
+        return nums[left];
+    }
+
+    public int singleNonDuplicateBinary(int[] nums) {
+        int left = 0;
+        int right = nums.length - 1;
+        while (left < right) {
+            int mid = (left + right) / 2;
+            if (nums[mid] != nums[mid - 1] && nums[mid] != nums[mid + 1]) {
+                return nums[mid];
+            }
+            boolean isOdd = (mid - left) % 2 == 1;
+            // 如果中间值是一对数字的左值
+            if (nums[mid] == nums[mid + 1]) {
+                if (isOdd) {
+                    // 1,1,2,2,3,4,4,5,5,6,6
+                    right = mid - 1;
+                } else {
+                    // 1,1,2,2,3
+                    left = mid + 2;
+                }
+            } else {
+                // 如果中间值是一对数字的右值
+                if (isOdd) {
+                    // 1,1,2,2,3,3,4,4,5,6,6
+                    left = mid + 1;
+                } else {
+                    // 1,1,2,3,3,4,4,5,5
+                    right = mid - 2;
+                }
+            }
+        }
+        return nums[left];
+    }
+
+    @DataProvider(name = "cases")
+    public Object[][] cases() {
+        Object[][] data = {
+                {new int[]{1, 1, 2, 3, 3, 4, 4, 8, 8}, 2},
+                {new int[]{3, 3, 7, 7, 10, 11, 11}, 10},
+                {new int[]{3, 3, 7}, 7},
+                {new int[]{3, 7, 7}, 3},
+
+        };
+        return data;
+    }
+
+    @Test(description = "单测", dataProvider = "cases")
+    public void test(int[] nums, int expected) {
+//        int res = singleNonDuplicate(nums);
+        int res = singleNonDuplicateBinary(nums);
+        Assert.assertEquals(res, expected);
+    }
+}
