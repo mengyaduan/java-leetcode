9202b288635139ed25998021b27099a3bc610329
yameng.dym <yameng.dym@autonavi.com>
2021-11-18 23:40:56 +0800

    update quickSort

--- src/QuickSort.java
+++ src/QuickSort.java
@@ -9,7 +9,43 @@ public class QuickSort {
     /**
      * 左闭右闭的上下限
      **/
-    public void quickSort(int[] nums, int l, int r) {
+    public void quickSortDoubleClose(int[] nums, int l, int r) {
+        int i = l;
+        int j = r;
+        int key = nums[i];
+        if (l >= r) {
+            return;
+        }
+        while (i < j) {
+            while (i < j && nums[j] >= key) {
+                j--;
+            }
+            if (i < j) {
+                nums[i] = nums[j];
+                i++;
+            }
+            while (i < j && nums[i] <= key) {
+                i++;
+            }
+            if (i < j) {
+                nums[j] = nums[i];
+                j--;
+            }
+        }
+        nums[i] = key;
+        for (int item : nums) {
+            System.out.print(item + " ");
+        }
+        System.out.println();
+        quickSortDoubleClose(nums, l, i - 1);
+        quickSortDoubleClose(nums, i + 1, r);
+
+    }
+
+    /**
+     * 左闭右闭的上下限
+     **/
+    public void quickSortHalfClose(int[] nums, int l, int r) {
         int i = l;
         int j = r - 1;
         int key = nums[i];
@@ -37,8 +73,8 @@ public class QuickSort {
             System.out.print(item + " ");
         }
         System.out.println();
-        quickSort(nums, l, i);
-        quickSort(nums, i + 1, r);
+        quickSortHalfClose(nums, l, i);
+        quickSortHalfClose(nums, i + 1, r);
 
     }
 
@@ -46,7 +82,10 @@ public class QuickSort {
     @Test(description = "")
     public void test() throws Exception {
         int[] nums = new int[]{5, 3, 7, 6, 4, 1, 0, 2, 9, 10, 8};
-        quickSort(nums, 0, nums.length);
+        System.out.println("左闭右闭");
+        quickSortDoubleClose(nums, 0, nums.length - 1);
+        System.out.println("左闭右开");
+        quickSortHalfClose(nums, 0, nums.length);
 
     }
 }
