76cee150db90673c74e17dc00e9bca533949cba7
yameng.dym <yameng.dym@autonavi.com>
2023-09-12 16:16:42 +0800

    update no121 47/50

--- src/Lc/dynamicprogramming/No121.java
+++ src/Lc/dynamicprogramming/No121.java
@@ -9,7 +9,7 @@ import org.testng.annotations.Test;
  **/
 public class No121 {
 
-    public int maxProfit(int[] prices) {
+    public int maxProfitComplicated(int[] prices) {
         int low = 0;
         int maxPro = 0;
         for (int i = 1; i < prices.length; i++) {
@@ -25,14 +25,27 @@ public class No121 {
         return maxPro;
     }
 
+    public int maxProfit(int[] prices) {
+        int[][] memo = new int[prices.length + 1][2];
+        // 第一天以前，收益必为0
+        memo[0][0] = 0;
+        memo[0][1] = Integer.MIN_VALUE;
+        for (int i = 1; i < prices.length + 1; i++) {
+            // stay 或 卖出
+            memo[i][0] = Math.max(memo[i - 1][0], memo[i - 1][1] + prices[i - 1]);
+            // stay 或 买入
+            memo[i][1] = Math.max(memo[i - 1][1], -prices[i - 1]);
+        }
+        return memo[prices.length][0];
+    }
+
 
     @DataProvider(name = "cases")
     public Object[][] cases() {
-        Object[][] data = {
+        return new Object[][]{
                 {new int[]{7, 1, 5, 3, 4, 6}, 5},
                 {new int[]{7, 2, 5, 3, 1, 6}, 5},
         };
-        return data;
     }
 
     @Test(description = "单测", dataProvider = "cases")
