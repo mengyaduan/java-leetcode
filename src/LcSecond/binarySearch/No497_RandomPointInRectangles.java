2003d38805300bab13a91c8ee016f0ec731f1701
yameng.dym <yameng.dym@autonavi.com>
2024-05-09 20:58:33 +0800

    draft

--- src/LcSecond/binarySearch/No497_RandomPointInRectangles.java
+++ src/LcSecond/binarySearch/No497_RandomPointInRectangles.java
@@ -2,6 +2,8 @@ package LcSecond.binarySearch;
 
 import org.testng.annotations.Test;
 
+import java.util.Random;
+
 /**
  * @see <a href="https://leetcode.com/problems/random-point-in-non-overlapping-rectangles/description/">在一堆长方形中找到随机的坐标</a>
  */
@@ -15,8 +17,11 @@ public class No497_RandomPointInRectangles {
 
     @Test(description = "")
     public void test() throws Exception {
-
-
+        int[][] rects = new int[][]{
+                {-2, -2, 1, 1}, {2, 2, 4, 6}
+        };
+        SolutionRecs solutionRecs = new SolutionRecs(rects);
+        solutionRecs.pick();
     }
 
 }
@@ -34,19 +39,38 @@ class SolutionRecs {
     public SolutionRecs(int[][] rects) {
         this.rects = rects;
         area = new int[rects.length];
-        for (int i = 0; i < rects.length; i++) {
-            area[i] = calculateArea(rects[i]);
+        area[0] = calculateArea(rects[0]);
+        sum = area[0];
+        for (int i = 1; i < rects.length; i++) {
+            area[i] = calculateArea(rects[i]) + area[i - 1];
             sum += area[i];
         }
     }
 
     public int[] pick() {
         int n = rects.length;
-
+        Random random = new Random();
+        int target = random.nextInt(sum);
+        // 通过二分法，定位到指定的长方形
+        int l = 0, r = n - 1;
+        int idx = 0;
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+            if (area[mid] > target) {
+                idx = mid;
+                r = mid - 1;
+            } else {
+                l = mid + 1;
+            }
+        }
+        // 找到长方形后，从里面ran一个点就行了
+        int[] rect = rects[idx];
         return null;
     }
 
     public int calculateArea(int[] rectangle) {
-        return 0;
+        int x = rectangle[2] - rectangle[0] + 1;
+        int y = rectangle[3] - rectangle[1] + 1;
+        return x * y;
     }
 }
